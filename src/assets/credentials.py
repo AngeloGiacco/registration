84c0e1b4aeab52b7459f37e43059d84bd1ffa778
Angelo Giacco <giacco.angelo.s@gmail.com>
2020-05-03 12:39:55 +0100

    Create credentials.py

---
+++ src/assets/credentials.py
@@ -0,0 +1,9 @@
+email = "" #add your email between the quotes, encode with base64 first to avoid shoulder surfers, this is not encryption
+password = "" # add your password between the quotes, encode this as well with base64, if a snooper sees this, they will not find out your password
+#https://www.base64encode.org/
+
+"""
+Disclaimer:
+Someone with tech expertise will see it is b64 encoding and may still be able to decode it. 
+This is not perfect security. It is not intended to be.
+"""
